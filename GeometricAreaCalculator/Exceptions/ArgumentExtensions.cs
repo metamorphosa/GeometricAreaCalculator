8559dd5996c606b3efbc935abbeff2b90e6370ac
r.chilaev <r.chilaev@itransition.com>
2022-11-06 13:50:40 +0300

    refactoring + gitignore

--- GeometricAreaCalculator/Exceptions/ArgumentExtensions.cs
+++ GeometricAreaCalculator/Exceptions/ArgumentExtensions.cs
@@ -7,10 +7,12 @@ public static class ArgumentExtensions
         var exception = message ?? $"{arg.ParamName} should be greater than {minValue}";
         return arg.Validate(arg.Value > minValue, exception);
     }
+    
     public static Validators.Argument<double> NotNegative(this Validators.Argument<double> arg)
     {
         return GreaterThan(arg, 0, $"{arg.ParamName} is negative");
     }
+    
     private static Validators.Argument<T> Validate<T>(this Validators.Argument<T> arg, bool condition, string message)
     {
         if (condition)
--- GeometricAreaCalculator/Shapes/Triangle.cs
+++ GeometricAreaCalculator/Shapes/Triangle.cs
@@ -5,9 +5,9 @@ namespace GeometricAreaCalculator.Shapes;
 
 public class Triangle : Shape
 {
-    private double A { get; set; }
-    private double B { get; set; }
-    private double C { get; set; }
+    public double A { get; set; }
+    public double B { get; set; }
+    public double C { get; set; }
     public bool IsRectangular { get; set; }
     
     public Triangle(double a, double b, double c)
